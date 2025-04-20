f366af90fcec4d3368822ce7bce90c9020bbf52c
vishnu <getvishnuvs@gmail.com>
2025-04-20 20:55:00 +0530

    completes delete,get ,edit controllers for not maker

--- Backend/src/controllers/notes/addNotes.ts
+++ Backend/src/controllers/notes/addNotes.ts
@@ -1,4 +1,4 @@
-import express,{Request,response} from "express";
+import express,{Request,Response} from "express";
 import Note from "../../models/Note";
 
 
@@ -17,5 +17,47 @@ export const addNote=async (req:Request,res:Response):Promise<void>=>{
 
   }catch(error){
     res.status(500).json({message:"Error saving note"})
+  }
+}
+
+
+export const getNotes = async(req:Request,res:Response)=>{
+   try{
+     const notes=  await Note.find();
+     res.json(notes);
+      
+
+   }catch(error){
+      res.status(500).json({message:"error in fething notes"})
+   }
+}
+
+export const deleteNote= async (req:Request,res:Response):Promise<void>=>{
+   try{
+     await Note.findByIdAndDelete(req.params.id)
+      res.status(204).end()
+
+   }catch(error){
+        res.status(500).json({message:"Error in deleting note"})
+   }
+}
+
+export const editNote= async(req:Request,res:Response):Promise<void>=>{
+  try{
+    const {title,content,subject} =req.body
+    const updatedNote= await Note.findByIdAndUpdate(
+      req.params.id,
+      {title,content,subject},
+      {new:true,runValidators:true}
+    );
+    if(!updatedNote){
+        res.status(404).json({message:"Note not found"})
+          return
+       
+    }
+    res.status(200).json(updatedNote);
+
+  }catch(error){
+   res.status(500).json({message:"Error updating note"})
   }
 }
